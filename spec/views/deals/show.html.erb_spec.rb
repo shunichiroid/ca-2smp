2d86353312142b9325800c68793c701977e4f77a
Shunichiro Ida <shunichiroid@gmail.com>
2018-05-08 17:56:45 +1000

    Reset uncommented match method

--- spec/views/deals/show.html.erb_spec.rb
+++ spec/views/deals/show.html.erb_spec.rb
@@ -16,13 +16,13 @@ RSpec.describe "deals/show", type: :view do
 
   it "renders attributes in <p>" do
     render
-    # expect(rendered).to match(/Title/)
-    # expect(rendered).to match(/MyText/)
-    # expect(rendered).to match(/MyText/)
-    # expect(rendered).to match(/9.99/)
-    # expect(rendered).to match(/9.99/)
-    # expect(rendered).to match(/2/)
-    # expect(rendered).to match(/3/)
-    # expect(rendered).to match(/Charge/)
+    expect(rendered).to match(/Title/)
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/9.99/)
+    expect(rendered).to match(/9.99/)
+    expect(rendered).to match(/2/)
+    expect(rendered).to match(/3/)
+    expect(rendered).to match(/Charge/)
   end
 end
