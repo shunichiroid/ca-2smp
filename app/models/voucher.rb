e195aa85f7e2c23771929526eaa2543de93e5925
Shunichiro Ida <shunichiroid@gmail.com>
2018-05-10 15:37:07 +1000

    Add uploader

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -2,4 +2,16 @@ class Voucher < ApplicationRecord
   belongs_to :deal
   belongs_to :user
 
+  include QRUploader::Attachment.new(:qrcode)
+  before_validation :generate_uuid, on: :create
+  before_validation :generate_qrcode, on: :create
+
+  def generate_uuid
+    self.uuid = SecureRandom.uuid
+  end
+
+  def generate_qrcode
+    
+  end
+
 end
---
+++ app/uploaders/qr_uploader.rb
@@ -0,0 +1,4 @@
+class QRUploader < Shrine
+    
+    
+end
